37403973498ea52fc8451a6dd1ddf872d473ee49
Sayed Nafisur Rahman Alif <143423207+SayedAliff@users.noreply.github.com>
2025-11-30 20:46:47 +0600

    V1.4.2

--- client/registration_system.py
+++ client/registration_system.py
@@ -3,16 +3,24 @@ import requests
 URL = "http://127.0.0.1:8000/reg"
 
 def menu():
-    print("\n--- Registration ---")
-    data = {
-        "member_id": input("Member ID: "),
-        "event_id": input("Event ID: ")
-    }
-    try:
-        res = requests.post(URL + "/", json=data)
-        if res.status_code == 200:
-            print("Registration Successful!")
-            print(res.json())
-        else:
-            print("Failed:", res.text)
-    except: print("Connection Error")
+    while True:
+        print("\n--- Registration ---")
+        print("1. Add Member for registration")
+        print("0. Back")
+        
+        choice = input("Choice: ")
+        if choice == '0': break
+        
+        if choice == '1':
+            data = {
+                "member_id": input("Member ID: "),
+                "event_id": input("Event ID: ")
+            }
+            try:
+                res = requests.post(URL + "/", json=data)
+                if res.status_code == 200:
+                    print("Registration Successful!")
+                    print(res.json())
+                else:
+                    print("Failed:", res.text)
+            except: print("Connection Error")
